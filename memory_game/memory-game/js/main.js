92b31198031bfb02dfdae3b579c06b698aff9434
Afkelly14 <62082898+Afkelly14@users.noreply.github.com>
2020-06-02 22:15:46 -0400

    Delete main.js

--- memory_game/memory-game/js/main.js
+++
@@ -1,7 +0,0 @@
-console.log("Up and Running");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "King";
-let cardFour = "King";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardFour);
